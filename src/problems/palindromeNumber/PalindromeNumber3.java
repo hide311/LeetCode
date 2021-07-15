9f4e984cadcd5453557e4d526d6ae27d5684c19c
hide311 <h.fukuhara311@gmail.com>
2021-07-15 22:19:25 +0900

    PalindromeNumber

---
+++ src/problems/palindromeNumber/PalindromeNumber3.java
@@ -0,0 +1,22 @@
+package problems.palindromeNumber;
+
+public class PalindromeNumber3 implements PalindromeNumber {
+    @Override
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String xStr = Integer.toString(x);
+        int fromRight = xStr.length();
+
+        for (int i = 0; i < xStr.length() / 2; i++) {
+            String strLeft = xStr.substring(i, i + 1);
+            String strRight = xStr.substring(fromRight - 1, fromRight);
+            if (!strLeft.equals(strRight)) {
+                return false;
+            }
+            fromRight--;
+        }
+        return true;
+    }
+}
